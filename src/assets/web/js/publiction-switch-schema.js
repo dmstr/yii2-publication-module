30339c18fa665e57ee6915c29032eac85b8da0ea
Elias Luhr <e.luhr@herzogkommunikation.de>
2018-01-25 09:49:34 +0100

    updated js

--- src/assets/web/js/publiction-switch-schema.js
+++ src/assets/web/js/publiction-switch-schema.js
@@ -1,7 +1,11 @@
-$("#publicationitem-publication_category_id").on('change.select2', function (e) {
-    if (confirm('If you change the JSON scheme, all changes will be lost. Continue?')) {
-        window.location = location.origin + location.pathname + `?PublicationItem[publication_category_id]=${$(this).val()}`
-    } else {
+let categorySelect = $("#publicationitem-publication_category_id")
+
+categorySelect.on('select2:selecting', function (e) {
+    if (!confirm('If you change the JSON scheme, all changes will be lost. Continue?')) {
         e.preventDefault()
     }
+});
+
+categorySelect.on('select2:select', function () {
+    window.location = location.origin + location.pathname + `?PublicationItem[publication_category_id]=${$(this).val()}`
 });
