5fe65f81fe65df86ea0f03b94b3ae4ae8ede02a5
Jan Reichl <j.reichl@herzogkommunikation.de>
2018-09-21 10:00:53 +0200

    cleanup dev-code, use addEventlistener load

--- src/assets/web/js/ck-init.js
+++ src/assets/web/js/ck-init.js
@@ -1,23 +1,4 @@
-
-// window.addEventListener('load', function () {
-//    var inputsList = document.querySelectorAll('[data-schemaformat="html"]');
-//    var inputs = [].slice.call(inputsList);
-//    inputs.forEach(function (input) {
-//        console.log(input.name);
-//        CKEDITOR.replace(input, CKCONFIG);
-//        CKEDITOR.instances[input.name].on('change', function () {
-//             var ckvalue = CKEDITOR.instances[input.name].getData();
-//            input.value = ckvalue;
-//            var evt = document.createEvent("HTMLEvents");
-//            evt.initEvent("change", false, true);
-//            input.dispatchEvent(evt);
-//        });
-//    });
-// });
-
-
-// triggers on load, saves values, works
-window.onload = function () {
+window.addEventListener('load', function () {
     var inputsList = document.querySelectorAll("textarea[data-schemaformat='html']");
     var inputs = [].slice.call(inputsList);
     inputs.forEach(function (input) {
@@ -31,5 +12,4 @@ window.onload = function () {
             input.dispatchEvent(evt);
         });
     })
-};
-
+});
