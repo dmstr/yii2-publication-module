54d2681d9abf6f992d7df0da94a6b4f2c782ea4b
Elias Luhr <e.luhr@herzogkommunikation.de>
2018-11-05 11:42:58 +0100

    Fixed major bug in json schema thingy

--- src/controllers/crud/PublicationItemController.php
+++ src/controllers/crud/PublicationItemController.php
@@ -6,6 +6,9 @@ namespace dmstr\modules\publication\controllers\crud;
 use dmstr\modules\publication\controllers\crud\actions\DeleteAttachment;
 use dmstr\modules\publication\models\crud\PublicationItem;
 use dmstr\modules\publication\models\crud\search\PublicationItem as PublicationItemSearch;
+use Yii;
+use yii\helpers\VarDumper;
+use yii\web\NotFoundHttpException;
 
 
 /**
@@ -44,6 +47,65 @@ class PublicationItemController extends BaseController
     {
         $actions = parent::actions();
         $actions['delete-tag-attachment'] = DeleteAttachment::class;
+        unset($actions['create'], $actions['update']);
         return $actions;
+    }
+
+    /**
+     * @return mixed|string|\yii\web\Response
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function actionCreate()
+    {
+        $model = new PublicationItem();
+
+        $publicationCategoryId = null;
+        if (isset(\Yii::$app->request->get()[$model->formName()]['category_id'])) {
+            $publicationCategoryId = \Yii::$app->request->get()[$model->formName()]['category_id'];
+        }
+        $model->setContentSchemaByCategoryId($publicationCategoryId);
+        $model->setTeaserSchemaByCategoryId($publicationCategoryId);
+        
+        try {
+            if ($model->load($_POST) && $model->save()) {
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
+
+            if (!\Yii::$app->request->isPost) {
+                $model->load($_GET);
+            }
+        } catch (\Exception $e) {
+            $msg = $e->errorInfo[2] ?? $e->getMessage();
+            $model->addError('_exception', $msg);
+        }
+        return $this->render($this->action->id, ['model' => $model]);
+    }
+
+    /**
+     * @param $id
+     * @return string|\yii\web\Response
+     * @throws \yii\base\InvalidParamException
+     * @throws \yii\web\HttpException
+     */
+    public function actionUpdate($id)
+    {
+        /** @var PublicationItem $model */
+        $model = PublicationItem::findOne($id);
+
+        if ($model === null) {
+            throw new NotFoundHttpException(Yii::t('publication', 'Publication item does not exist'));
+        }
+
+        $model->setContentSchemaByCategoryId($model->category_id);
+        $model->setTeaserSchemaByCategoryId($model->category_id);
+
+        if ($model->load($_POST) && $model->save()) {
+
+            return $this->redirect(Url::previous());
+        }
+
+        return $this->render($this->action->id, [
+            'model' => $model,
+        ]);
     }
 }
