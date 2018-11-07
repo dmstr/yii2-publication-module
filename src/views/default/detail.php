46f3998832b2c430a77136bbcae9362e68d1217a
Elias Luhr <e.luhr@herzogkommunikation.de>
2018-11-07 09:16:47 +0100

    updated classes

--- src/views/default/detail.php
+++ src/views/default/detail.php
@@ -10,7 +10,7 @@ use hrzg\widget\widgets\Cell;
 
 $this->title = Yii::$app->settings->get('site', 'publication.default-detail') ?: Yii::t('publication', 'Detail');
 ?>
-<div class="publication-default-index">
+<div class="publication-default-detail">
     <?= Cell::widget(['id' => 'publication_detail_top', 'requestParam' => 'publication_detail_top']) ?>
     <?= Cell::widget(['id' => 'publication_detail_top_' . $item->id, 'requestParam' => 'publication_detail_top_' . $item->id]) ?>
     <?= Publication::widget(['item' => $item, 'teaser' => false]) ?>
--- src/views/default/tag.php
+++ src/views/default/tag.php
@@ -11,7 +11,7 @@ use hrzg\widget\widgets\Cell;
 
 $this->title = Yii::$app->settings->get('site', 'publication.default-tag') ?: Yii::t('publication', 'Overview');
 ?>
-<div class="publication-default-tag">
+<div class="publication-default-tag publication-default-index">
     <?= Cell::widget(['id' => 'publication_tag_top', 'requestParam' => 'publication_tag_top']) ?>
     <?= TaggedPublication::widget(['tagId' => $tagId, 'limit' => $limit]) ?>
     <?= Cell::widget(['id' => 'publication_tag_bottom', 'requestParam' => 'publication_tag_bottom']) ?>
