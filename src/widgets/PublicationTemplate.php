c47fedbb7e8d7e4285b2c2924d3026cd8afb289c
Elias Luhr <e.luhr@herzogkommunikation.de>
2018-11-19 14:18:08 +0100

    added php template

---
+++ src/widgets/PublicationTemplate.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @link http://www.diemeisterei.de/
+ * @copyright Copyright (c) 2018 diemeisterei GmbH, Stuttgart
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace dmstr\modules\publication\widgets;
+
+
+use yii\base\Widget;
+
+/**
+ * Class PublicationTemplate
+ * @package dmstr\modules\publication\widgets
+ * @author Elias Luhr <e.luhr@herzogkommunikation.de>
+ */
+class PublicationTemplate extends Widget
+{
+
+}
