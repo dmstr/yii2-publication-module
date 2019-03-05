8430ed6b149b0c438dff0d9d600455d50b6f8e9e
Jens Giessmann <j.giessmann@herzogkommunikation.de>
2019-03-05 10:59:22 +0100

    refactor models\crud\search\PublicationItem query builder for proper fallback language handling and to be able to sort by title and date

--- src/models/crud/search/PublicationItem.php
+++ src/models/crud/search/PublicationItem.php
@@ -53,37 +53,70 @@ class PublicationItem extends PublicationItemModel
      */
     public function search($params)
     {
-        $model_table = PublicationItemModel::tableName();
-        $translation_table = PublicationItemTranslation::tableName();
-        $meta_table = PublicationItemMeta::tableName();
+
+        // get the raw table names as we need to build constraints as string, see leftJoins below
+        $model_table = Yii::$app->db->schema->getRawTableName(PublicationItemModel::tableName());
+        $translation_table = Yii::$app->db->schema->getRawTableName(PublicationItemTranslation::tableName());
+        $meta_table = Yii::$app->db->schema->getRawTableName(PublicationItemMeta::tableName());
 
         $query = PublicationItemModel::find();
 
-        $query->joinWith('translations');
-        $query->joinWith('metas');
-        $query->andWhere(['OR',
-                [$translation_table . '.language' => \Yii::$app->language],
-                isset(\Yii::$app->params['fallbackLanguages'][\Yii::$app->language]) ? [$translation_table . '.language' => \Yii::$app->params['fallbackLanguages'][\Yii::$app->language]] : '']
-        );
-        $query->andWhere(['OR',
-                [$meta_table . '.language' => \Yii::$app->language],
-                isset(\Yii::$app->params['fallbackLanguages'][\Yii::$app->language]) ? [$meta_table . '.language' => \Yii::$app->params['fallbackLanguages'][\Yii::$app->language]] : '']
+        $query->select(["{$model_table}.*", 'meta.release_date as release_date', 'trans.title as title']);
+
+        if (!empty(\Yii::$app->params['fallbackLanguages'][\Yii::$app->language])) {
+            $query->select(["{$model_table}.*", 'COALESCE(meta.release_date, fbmeta.release_date) as release_date', 'COALESCE(trans.title, fbtrans.title) as title']);
+            $query->leftJoin(
+                "$translation_table as fbtrans",
+                [
+                    'AND',
+                    "{$model_table}.id = fbtrans.item_id",
+                     ['fbtrans.language' => \Yii::$app->params['fallbackLanguages'][\Yii::$app->language]]
+                ]
+            );
+
+            $query->leftJoin(
+                "$meta_table as fbmeta",
+                [
+                    'AND',
+                    "{$model_table}.id = fbmeta.item_id",
+                    ['fbmeta.language' => \Yii::$app->params['fallbackLanguages'][\Yii::$app->language]]
+                ]
+            );
+
+        }
+
+        // the column constraint must be definied as string, otherwise the right side will be quoted as string
+        // see: https://www.yiiframework.com/doc/guide/2.0/en/db-query-builder#join
+        $query->leftJoin(
+            "$translation_table as trans",
+            [
+                'AND',
+                "{$model_table}.id = trans.item_id",
+                ['trans.language' => \Yii::$app->language]
+            ]
         );
 
-        $query->groupBy([$model_table . '.id']);
+        $query->leftJoin(
+            "$meta_table as meta",
+            [
+                'AND',
+                "{$model_table}.id = meta.item_id",
+                ['meta.language' => \Yii::$app->language]
+            ]
+        );
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
             'sort' => [
                 'attributes' => [
-//                    'title' => [
-//                        'asc' => [$translation_table . '.title' => SORT_ASC],
-//                        'desc' => [$translation_table . '.title' => SORT_DESC]
-//                    ],
-//                    'release_date' => [
-//                        'asc' => [$meta_table . '.release_date' => SORT_ASC],
-//                        'desc' => [$meta_table . '.release_date' => SORT_DESC]
-//                    ],
+                    'title' => [
+                        'asc' => ['title' => SORT_ASC],
+                        'desc' => ['title' => SORT_DESC]
+                    ],
+                    'release_date' => [
+                        'asc' => ['release_date' => SORT_ASC],
+                        'desc' => ['release_date' => SORT_DESC]
+                    ],
                     'category_id' => [
                         'asc' => [$model_table . '.category_id' => SORT_ASC],
                         'desc' => [$model_table . '.category_id' => SORT_DESC]
@@ -112,10 +145,12 @@ class PublicationItem extends PublicationItemModel
 
         $query->andFilterWhere([$model_table . '.id' => $this->id]);
         $query->andFilterWhere([$model_table . '.category_id' => $this->category_id]);
-        $query->andFilterWhere(['LIKE', $translation_table . '.title', $this->title]);
+        if (!empty($this->title)) {
+            $query->andFilterHaving(['LIKE', 'title', $this->title]);
+        }
 
         if (!empty($this->release_date)) {
-            $query->andFilterWhere([$meta_table . '.release_date' => date('Y-m-d H:i:s', strtotime($this->release_date))]);
+            $query->andFilterHaving(['release_date' => date('Y-m-d H:i:s', strtotime($this->release_date))]);
         }
 
 
