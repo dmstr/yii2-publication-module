2b5338b42eb5044cb5e1c5d00a78a5c371f4c00f
Elias Luhr <e.luhr@herzogkommunikation.de>
2023-06-13 15:37:20 +0200

    Hide widgets cells in detail by module property

--- src/views/default/detail.php
+++ src/views/default/detail.php
@@ -8,6 +8,8 @@
 use dmstr\modules\publication\widgets\Publication;
 use hrzg\widget\widgets\Cell;
 
+$showWidgetCells = $this->context->module->showWidgetCellsInDetailView;
+
 if (!empty($item->title)) {
     $title  = $item->title;
 } else {
@@ -16,9 +18,16 @@ if (!empty($item->title)) {
 $this->title = htmlspecialchars($title, ENT_NOQUOTES, Yii::$app ? Yii::$app->charset : 'UTF-8', false);
 ?>
 <div class="publication-default-detail">
-    <?= Cell::widget(['id' => 'publication_detail_top', 'requestParam' => 'publication_detail_top']) ?>
-    <?= Cell::widget(['id' => 'publication_detail_top_' . $item->id, 'requestParam' => 'publication_detail_top_' . $item->id]) ?>
-    <?= Publication::widget(['item' => $item, 'teaser' => false]) ?>
-    <?= Cell::widget(['id' => 'publication_detail_bottom_' . $item->id, 'requestParam' => 'publication_detail_bottom_' . $item->id]) ?>
-    <?= Cell::widget(['id' => 'publication_detail_bottom', 'requestParam' => 'publication_detail_bottom']) ?>
+
+    <?php
+    if ($showWidgetCells) {
+        echo Cell::widget(['id' => 'publication_detail_top', 'requestParam' => 'publication_detail_top']);
+        echo Cell::widget(['id' => 'publication_detail_top_' . $item->id, 'requestParam' => 'publication_detail_top_' . $item->id]);
+    }
+    echo Publication::widget(['item' => $item, 'teaser' => false]);
+    if ($showWidgetCells) {
+        echo Cell::widget(['id' => 'publication_detail_bottom_' . $item->id, 'requestParam' => 'publication_detail_bottom_' . $item->id]);
+        echo Cell::widget(['id' => 'publication_detail_bottom', 'requestParam' => 'publication_detail_bottom']);
+    }
+    ?>
 </div>
